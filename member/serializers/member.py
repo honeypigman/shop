5570109bb73238b46474abf911b0043c2b52873d
honeypigman <honeypigman@gmail.com>
2023-03-09 02:14:24 +0900

    SHOP0308 회원가입, 중복 기본 API

--- member/serializers/member.py
+++ member/serializers/member.py
@@ -1,13 +1,23 @@
 from rest_framework import serializers
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
 from member.const import MemberActiveCode
 from member.models.base import Member
 
 
 class MemberSerializer(serializers.ModelSerializer):
-    id = serializers.CharField(read_only=True)
+    id = serializers.CharField(required=True)
     password = serializers.CharField(write_only=True)
     active_code = serializers.ChoiceField(choices=MemberActiveCode, default=MemberActiveCode.ACTIVE)
     create_date = serializers.DateTimeField()
+
+    def validate(self, data):
+        if Member.objects.filter(id=data.get('id')):
+            raise ValidationError(detail='회원ID중복', code=status.HTTP_400_BAD_REQUEST)
+        return data
+
+    def create(self, validated_data):
+        return Member.objects.create(**validated_data)
 
     class Meta:
         model = Member
